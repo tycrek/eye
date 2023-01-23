03ba275df42a71d2d96f7c35b716a882f177fdd5
tycrek <gpg+github@jmoore.dev>
2023-01-22 20:13:28 -0700

    feat: imeplement bearer auth

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Hono } from 'hono';
+import { bearerAuth } from 'hono/bearer-auth';
 const app = new Hono();
 
 app.get('/api', (ctx) => ctx.text('Welcome to the API'));
+
+app // Bearer auth for KV
+	.use('/api/kv/*', (ctx, next) => bearerAuth({ token: ctx.env.TOKEN })(ctx, next))
 app.get('/*', (ctx) => (ctx.env.ASSETS as Fetcher).fetch(ctx.req));
 
 export default app;
