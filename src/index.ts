e17c4b94b3c85fc40076387c5ff536788d5df771
tycrek <gpg+github@jmoore.dev>
2023-01-22 19:12:35 -0700

    feat: render static html index

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { Hono } from 'hono';
 const app = new Hono();
 
 app.get('/api', (ctx) => ctx.text('Welcome to the API'));
+app.get('/*', (ctx) => (ctx.env.ASSETS as Fetcher).fetch(ctx.req));
 
 export default app;
