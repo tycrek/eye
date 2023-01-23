d065f652a504cf8fbc20f1f0343c5650341efd9b
tycrek <gpg+github@jmoore.dev>
2023-01-22 20:13:41 -0700

    feat: implement KV get/set with bearer auth

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,19 @@ app.get('/api', (ctx) => ctx.text('Welcome to the API'));
 
 app // Bearer auth for KV
 	.use('/api/kv/*', (ctx, next) => bearerAuth({ token: ctx.env.TOKEN })(ctx, next))
+
+	// Get KV value
+	.get('/api/kv/:key', (ctx) =>
+		(ctx.env.eye as KVNamespace).get(ctx.req.param().key)
+			.then((value) => ctx.json({ value }))
+			.catch((err) => (ctx.status(400), ctx.json({ error: err.message }))))
+
+	// Set KV value
+	.post('/api/kv/:key/:value', (ctx) =>
+		(ctx.env.eye as KVNamespace).put(ctx.req.param().key, ctx.req.param().value)
+			.then(() => ctx.json({ success: true }))
+			.catch((err) => (ctx.status(400), ctx.json({ error: err.message }))));
+
 app.get('/*', (ctx) => (ctx.env.ASSETS as Fetcher).fetch(ctx.req));
 
 export default app;
